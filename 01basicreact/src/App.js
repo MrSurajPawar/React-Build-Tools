0f015b0fdca2ea00baef6606a582349dc6e7e431
MrSurajPawar <surajspawar007@gmail.com>
2024-04-23 18:11:00 +0530

    Create react app file structure

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./Chai.js";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h2>hello world</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+//the 'Create react app' allow both extension ie .js & .jsx , but the 'Vite' only strictly allow the .jsx
+export default function Chai() {
+    return (
+        <h1>This is Chai</h1>
+    );
+}
