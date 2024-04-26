209b400d7a492bf83ffe80f96e91f61ebc22dbaf
MrSurajPawar <surajspawar007@gmail.com>
2024-04-26 09:17:17 +0530

    Customizing React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,9 @@
 import Chai from "./chai";
 function App() {
+  const username = 'Suraj'
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World this is {username}</h1>
       <Chai />
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+function MyApp() {
+    return (
+        <div>
+            <h1>Chai aur react !</h1>
+        </div>
+    );
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit the google'
+// }
+
+const anotherElement = (
+    <a href="https://www.google.com" target='_blank'>visit google</a>
 );
+
+const anotheruser = 'Suraj'
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://www.google.com' , target: '_blank'},
+    'click me to visit the google',
+    anotheruser
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
+);  
